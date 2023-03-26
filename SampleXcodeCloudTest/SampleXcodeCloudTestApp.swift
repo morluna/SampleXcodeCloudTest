40927969b62cc2232bfcc0cccfdce6ca7f14ccb3
Marcos Ortiz <marcos.o@mangolytics.com>
2023-03-25 19:14:08 -0600

    Initial Commit

---
+++ SampleXcodeCloudTest/SampleXcodeCloudTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleXcodeCloudTestApp.swift
+//  SampleXcodeCloudTest
+//
+//  Created by Marcos on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleXcodeCloudTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
