3961231d37c65e5b88df3a2967efc27f3d7659aa
Marcos Ortiz <marcos.o@mangolytics.com>
2023-03-25 23:03:56 -0600

    🔨 Added echo command in script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo 🌴 Current Branch: $CI_BRANCH
+
 # If we are in a release branch, we assume we're following a naming pattern such as `release-1.0.0`
 # We want to extract this version and set the version number of our project this way.
 if [[ $CI_BRANCH == release/* ]]; then
