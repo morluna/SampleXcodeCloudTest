5c84178c6ddfd84e707147a6f74db8e885e50b51
Marcos Ortiz <marcos.o@mangolytics.com>
2023-03-26 09:58:21 -0600

    Improve custom ci scripts

--- ci_scripts/Helpers/release_version_bump.sh
+++ ci_scripts/Helpers/release_version_bump.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# CI_XCODE_PROJECT = SampleXcodeCloudTest.xcodeproj
+# CI_BRANCH = release/1.0.0
+
 set -e
 
+# Given our branch name is `release/1.0.0` this will strip out the prefix,
+# leaving RELEASE_VERSION = 1.0.0
 RELEASE_VERSION=${CI_BRANCH#release/}
 
 cd $CI_WORKSPACE
