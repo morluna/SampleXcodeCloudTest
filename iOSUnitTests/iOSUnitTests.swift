1f50a1525ccfa9706eb331569cb82fa0acde9905
Marcos Ortiz <marcos.o@mangolytics.com>
2023-03-25 21:59:03 -0600

    Added app icon and test suites

---
+++ iOSUnitTests/iOSUnitTests.swift
@@ -0,0 +1,14 @@
+//
+//  iOSUnitTests.swift
+//  iOSUnitTests
+//
+//  Created by Marcos on 3/25/23.
+//
+
+import XCTest
+
+final class iOSUnitTests: XCTestCase {
+    func testTwoPlusTwoEqualsFour() throws {
+        XCTAssertEqual(2 + 2, 4)
+    }
+}
---
+++ macOSUnitTests/macOSUnitTests.swift
@@ -0,0 +1,14 @@
+//
+//  macOSUnitTests.swift
+//  macOSUnitTests
+//
+//  Created by Marcos on 3/25/23.
+//
+
+import XCTest
+
+final class macOSUnitTests: XCTestCase {
+    func testTwoPlusTwoEqualsFour() throws {
+        XCTAssertEqual(2 + 2, 4)
+    }
+}
